0c6c5641e61371c79fcb7b2959a6419fc0cbcf7e
Brett Christiansen <wbc2025@nyu.edu>
2022-04-10 22:01:41 -0500

    first attempt

--- solution.py
+++ solution.py
@@ -5,7 +5,11 @@ import struct
 import time
 import select
 import binascii
+import random
 # Should use stdev
+from statistics import stdev
+from warnings import catch_warnings
+from socket import gaierror
 
 ICMP_ECHO_REQUEST = 8
 
@@ -103,22 +107,55 @@ def doOnePing(destAddr, timeout):
 def ping(host, timeout=1):
     # timeout=1 means: If one second goes by without a reply from the server,  	
     # the client assumes that either the client's ping or the server's pong is lost
-    dest = gethostbyname(host)
-    print("Pinging " + dest + " using Python:")
-    print("")
+    
+    dest = ''
+
+    try:
+        dest = gethostbyname(host)
+        print("Pinging " + dest + " using Python:")
+        print("")
+    except gaierror as ex:
+        print(ex)
+        return ['0', '0.0', '0', '0.0']
     
     #Send ping requests to a server separated by approximately one second
     #Add something here to collect the delays of each ping in a list so you can calculate vars after your ping
     
+    packet_min = 1000
+    packet_max = 0
+    packet_sum = 0
+    stdev_var = []
+
     for i in range(0,4): #Four pings will be sent (loop runs for i=0, 1, 2, 3)
-        delay = doOnePing(dest, timeout)
+        # delay = doOnePing(dest, timeout)
+
+        delay = random.randint(1, 999)
+        # delay = 0
+
         print(delay)
+
+        packet_sum += delay
+        stdev_var.append(delay)
+
+        if delay < packet_min:
+            packet_min = delay
+
+        if delay > packet_max:
+            packet_max = delay
+
+
         time.sleep(1)  # one second
         
+    packet_avg = packet_sum / 4
+
     #You should have the values of delay for each ping here; fill in calculation for packet_min, packet_avg, packet_max, and stdev
-    #vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
+    vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
 
     return vars
 
 if __name__ == '__main__':
-    ping("google.co.il")
+    vars = ping("google.co.il")
+    print(vars)
+
+    vars = ping("No.no.e")
+    print(vars)
